01fe7871eeff9402615d4a911519c12f082c9e96
KeithAGang <keithgangarahwe@gmail.com>
2024-01-04 14:35:20 +0200

    Updated Project

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return ( 
+        <div>
+            Login Page🔏🔏🔏
+        </div>
+     );
+}
+ 
+export default LoginPage;
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+
+const state = true;
+
+export default function Home() {
+  return (
+    <div >
+      This Is A Protected Route!
+    </div>
+  )
+    
+}
